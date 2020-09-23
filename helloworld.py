9ad944819058927ef2142274c354491c70152dec
Yogiraj <yogiraj.kadikar@gmail.com>
2020-09-24 01:32:19 +0530

    host added

--- helloworld.py
+++ helloworld.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def helloworld():
+def hello_world():
     return render_template("index.html")
 
 if __name__ == "__main__":
